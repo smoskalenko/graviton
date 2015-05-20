593693bc57d2ee8add20ab152a54ddbec9b1c5a7
Lucas Bickel <lucas.bickel@swisscom.com>
2015-05-20 16:40:08 +0200

    Implement postAction
    I'm expecting this to still have some errors...

--- src/Graviton/PersonBundle/Controller/AbstractCustomerController.php
+++ src/Graviton/PersonBundle/Controller/AbstractCustomerController.php
@@ -44,26 +44,52 @@ abstract class AbstractCustomerController extends RestController
     /**
      * save data to diff collection on post
      *
-     * @param Request $request client request 
+     * @param Request $request client request
      *
      * @return Response $response
      */
     public function postAction(Request $request)
     {
-        // grab request and check data
+        // grab request and check data against original schema
         $response = $this->getResponse();
         $record = $this->deserialize(
-            $request->getContent() //, add document class here!
+            $request->getContent(),
+            'GravitonDyn\CustomerBundle\Document\Customer'
         );
 
         // create diff object
-        $documentClass = $this->diffRepo->getDocumentClass;
-
+        $documentClass = $this->diffRepo->getClassName();
         $diff = new $documentClass;
-        $diff->setOld(null);
+        $diff->setOriginal(null);
+        $diff->setNew($record);
 
+        // save diff data
         $manager = $this->diffRepo->getDocumentManager();
         $manager->persist($diff);
         $manager->flush();
+        $diff = $this->diffRepo->find($diff->getId());
+
+        // Set status code and content
+        $response->setStatusCode(Response::HTTP_CREATED);
+        $response->setContent($this->serialize($diff->getNew()));
+ 
+        $routeName = $request->get('_route');
+        $routeParts = explode('.', $routeName);
+        $routeType = end($routeParts);
+
+        if ($routeType == 'post') {
+            $routeName = substr($routeName, 0, -4) . 'get';
+        }
+
+        $response->headers->set(
+            'Location',
+            $this->getRouter()->generate($routeName, array('id' => $record->getId()))
+        );
+
+        return $this->render(
+            'GravitonRestBundle:Main:index.json.twig',
+            array('response' => $response->getContent()),
+            $response
+        );
     }
 }
